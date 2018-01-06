428f83a8530e3a2fdd4da2b70796522205369d55
wantedHorizon <33939097+wantedHorizon@users.noreply.github.com>
2018-01-06 21:41:26 +0200

    Add files via upload

--- demo/Test.java
+++ demo/Test.java
@@ -1,19 +1,22 @@
 package demo;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Test {
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       united test1=new united();
-     //  Csv2Kml x=new Csv2Kml("c:\\check\\unitedCsv.csv","c:\\check\\k.kml");
-		/*
-		String time="28/10/2017 20:31";
-		Csv2Kml x=new Csv2Kml();
-		time=x.Timecorrect(time);
-		System.out.println(time);*/
-
+		//UnitedOrginizer demo =new UnitedOrginizer("c:\\knew\\unitedCSV.csv");
+      united test1=new united();//using enterface 
+		//FirstALGO x=new FirstALGO(orgLocation,outputName);
+    //SecondALGO x=new SecondALGO(comb/united file,TestFile location,output);
+      
 	}
 
 }
